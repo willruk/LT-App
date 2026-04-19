ce67aa4f56ab6cc2d231dcc35930ef2679ee0252
willruk <william.rowe@hotmail.co.uk>
2026-04-19 01:38:04 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 console.log("Life Tracks loaded");
+console.log("Spotify search URL:", links.spotify);
 
 var birthdayInput = document.getElementById("birthday");
 var goButton = document.getElementById("go");
